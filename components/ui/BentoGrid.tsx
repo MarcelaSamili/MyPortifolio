512d6ea330e490a8e830ee665d1933b76a19005a
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-02-20 16:09:25 -0300

    Finish the bentoGrid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,13 @@
+'use client';
 import { cn } from '@/utils/cn';
 import { BackgroundGradientAnimation } from './BgGradientAnimation';
 import { VortexDemo } from './VoertexGid';
+import Lottie from 'react-lottie';
+import { useState } from 'react';
+import animationData from '@/data/confetti.json';
+import MagicButton from './MagicButton';
+import { IoCopyOutline } from 'react-icons/io5';
+/*-------------------------------------*/
 
 export const BentoGrid = ({
   className,
@@ -40,10 +47,18 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setcopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('marcela.barroso10.s@gmail.com');
+
+    setcopied(true);
+  };
+
   return (
     <div
       className={cn(
-        'row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500',
+        'row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border-white/[0.1]',
         className
       )}
       style={{
@@ -52,7 +67,7 @@ export const BentoGridItem = ({
           'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(26,9,121,1) 35%, rgba(89,0,255,1) 100%)',
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -68,20 +83,24 @@ export const BentoGridItem = ({
             id === 5 && 'w-ful opacity-80'
           }`}
         >
-          {' '}
           {spareImg && (
             <img
               src={spareImg}
               alt={spareImg}
-              className="object-cover object-center"
+              className="object-cover object-center w-full h-full"
             />
           )}
         </div>
+
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+            <div
+              className="absolute z-50 flex items-center justify-center
+           text-white font-bold"
+            />
           </BackgroundGradientAnimation>
         )}
+
         <div
           className={cn(
             (titleClassName =
@@ -94,9 +113,60 @@ export const BentoGridItem = ({
           <div className="font-sans font-bol text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
-        </div>
 
-        {id === 2 && <VortexDemo />}
+          {id === 2 && <VortexDemo />}
+
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {['NextJs', 'ReactJs', 'TypeScript', 'NodeJs'].map(item => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+                {['JavaScript', 'API', 'MongoDB', 'MySQL'].map(item => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData: animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: 'xMidYMid slice',
+                    },
+                  }}
+                />
+              </div>
+              <MagicButton
+                title={copied ? 'Email copied' : 'Copy Email'}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- components/ui/VoertexGid.tsx
+++ components/ui/VoertexGid.tsx
@@ -3,22 +3,11 @@ import { Vortex } from '../ui/vortex';
 
 export function VortexDemo() {
   return (
-    <div
-      className="relative flex flex-col 
-    justify-center items-center rounded-md overflow-hidden "
-    >
+    <div className="w-full h-full absolute rounded-lg  top-0 left-0 overflow-hidden ">
       <Vortex
         backgroundColor="black"
-        className="flex items-center flex-col justify-center"
-      >
-        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-          Nextjs Intusiastic
-        </h2>
-        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          An Brazilian Full-Stark Developer
-        </p>
-      </Vortex>
+        className="items-center flex-col justify-center"
+      ></Vortex>
     </div>
   );
 }
-/*w-[calc(100%-4rem)] mx-auto rounded-md h-screen overflow-hidden  */
