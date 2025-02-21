ce71c36cced10defa71dcd064dfc41c929fcc535
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-02-21 16:29:58 -0300

    Experience section

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -4,7 +4,7 @@ import { InfiniteMovingCards } from './ui/infinite-moving-cards';
 
 const Clients = () => {
   return (
-    <div className="py-20" id="projects">
+    <div className="py-20" id="testimonials">
       <h1 className="heading">
         Links words from <span className="text-purple"> satisfied clients</span>
       </h1>
@@ -15,7 +15,7 @@ const Clients = () => {
           direction="right"
           speed="slow"
         />
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16">
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map(({ id, img, name, nameImg }) => (
             <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
               <img src={img} alt={name} className="md:w-10 w-5" />
