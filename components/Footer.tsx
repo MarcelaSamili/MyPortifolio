00593afa13563dde85a49bde22e955a97cdac3e7
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-02-24 16:12:55 -0300

    Making the footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+      <div>
+        <img src="" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
